e1ed9c8c6a17731789f3b67159c013dac5bf6313
Velpula Sai Kiran Yadav <saikiranyadavvelpula.001@gmail.com>
2024-08-05 01:25:17 +0530

    fixed password input

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -1,19 +1,17 @@
 import { useState } from "react";
-
 import api from "../api";
 import { useNavigate } from "react-router-dom";
 import { ACCESS_TOKEN, REFRESH_TOKEN } from "../constants";
-
 import "../styles/Form.css"
 import LoadingIndicator from "./LoadingIndicator";
 
-function Form({route, method}) {
+function Form({ route, method }) {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const [loading, setLoading] = useState(false);
     const navigate = useNavigate();
 
-    const name = method === "login" ? "Login": "Register";
+    const name = method === "login" ? "Login" : "Register";
 
     const handleSubmit = async (e) => {
         setLoading(true);
@@ -35,27 +33,29 @@ function Form({route, method}) {
         }
     };
 
-    return <form onSubmit={handleSubmit} className="form-container">
-        <h1>{name}</h1>
-        <input
-            className="form-input"
-            type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            placeholder="Username"
-        />
-        <input
-            className="form-input"
-            type="password"
-            value={password}
-            onChange={(e) => setUsername(e.target.value)}
-            placeholder="Password"
-        />
-        {loading && <LoadingIndicator />}
-        <button className="form-button" type="submit">
-            {name}
-        </button>
-    </form>
+    return (
+        <form onSubmit={handleSubmit} className="form-container">
+            <h1>{name}</h1>
+            <input
+                className="form-input"
+                type="text"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                placeholder="Username"
+            />
+            <input
+                className="form-input"
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Password"
+            />
+            {loading && <LoadingIndicator />}
+            <button className="form-button" type="submit">
+                {name}
+            </button>
+        </form>
+    );
 }
 
 export default Form
