b50d9e4affe988a206d8100404e86fa2b1550bc7
Velpula Sai Kiran Yadav <saikiranyadavvelpula.001@gmail.com>
2024-08-04 19:09:58 +0530

    view for creating users

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from  rest_framework import generics
+from .serializers import UserSerializer
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
 # Create your views here.
+
+class CreateUserView(generics.CreateAPIView):
+    query_set = User.objects.all() # providing all the user info, while creating user, so we can't repeat.
+    serializer_class = UserSerializer # what kind of data should be serialized.
+    permission_class = [AllowAny] # permission to anyone.
