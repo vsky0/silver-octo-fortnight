9ce2ebc39a87b52e9d150b43435d5bf801b8245c
Velpula Sai Kiran Yadav <saikiranyadavvelpula.001@gmail.com>
2024-08-05 01:18:35 +0530

    fixed url route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,9 @@
-import react from 'react'
-import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom"
-
+import react from "react"
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
 import Login from "./pages/Login"
 import Register from "./pages/Register"
 import Home from "./pages/Home"
 import NotFound from "./pages/NotFound"
-
 import ProtectedRoute from "./components/ProtectedRoute"
 
 function Logout() {
@@ -18,20 +16,19 @@ function RegisterAndLogout() {
   return <Register />
 }
 
-
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route
-          path='/'
+          path="/"
           element={
             <ProtectedRoute>
               <Home />
             </ProtectedRoute>
           }
         />
-        <Route path='/login' element={<Login />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/logout" element={<Logout />} />
         <Route path="/register" element={<RegisterAndLogout />} />
         <Route path="*" element={<NotFound />}></Route>
